e30d3ba90d3afbbfaf9bb6ab5a2beaf661fddbce
Mitja Felicijan <m@mitjafelicijan.com>
2023-10-03 03:33:15 +0200

    Added target mana bar color

--- LighterBlueManaBar.lua
+++ LighterBlueManaBar.lua
@@ -5,9 +5,11 @@ end
 
 local frame = CreateFrame("Frame")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("PLAYER_TARGET_CHANGED")
 frame:SetScript("OnEvent", function(self, event)
-	if event == "PLAYER_ENTERING_WORLD" then
-		ChangeManaBarColor()
+	if event == "PLAYER_ENTERING_WORLD" or event == "PLAYER_TARGET_CHANGED" then
+		ChangeManaBarColor(PlayerFrameManaBar)
+		ChangeManaBarColor(TargetFrameManaBar)
 	end
 end)
 
